0c15e106d8a05c84abb6b123f267d55be207e5b8
Peaces-a <444999945@qq.com>
2023-01-19 20:29:13 +0800

    Site updated: 2023-01-19 20:29:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/9daba997.html","posts/2de62673.html","posts/4a17b156.html","posts/3b4b656d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/2de62673.html","posts/9daba997.html","posts/b9e80aa0.html","posts/3b4b656d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
